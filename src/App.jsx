584ff09185666a7a1cf70f16ef98cca34ffe8849
aiamanme <amanme9285@gmail.com>
2023-08-04 22:13:37 +0530

    sapatread componants

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
+function CandyButton() {
   const [candys, setCandys] = useState(0);
   function addCandys() {
     setCandys(candys + 1);
   }
 
+  
+  return (
+    <button onClick={addCandys}>
+      Candy candys : <span id="candy-value">{candys}</span>
+    </button>
+  );
+}
+
+export default function App() {
   return (
     <div id="main">
       <h1>
         Hi 👋, My name is Luffy and i like
         <br /> candeys 🍬 a lot!
       </h1>
-      <button onClick={addCandys}>
-        Candy candys : <span id="candy-value">{candys}</span>
-      </button>
+      <CandyButton />
     </div>
   );
 }
-
-export default App;
